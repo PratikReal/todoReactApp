143fc323d8fd094d74aaf754ad2e1a94e3a368b8
PratikBaraskar <PratikBaraskar123@gmail.com>
2020-10-04 16:36:08 +0530

    Updates

---
+++ precache-manifest.4389863bf2d6d305c057d604c8bc8001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894fb5256aab1286a37359b76b5a2fac",
+    "url": "/todoReactApp/index.html"
+  },
+  {
+    "revision": "9a8617376c5734e5aac9",
+    "url": "/todoReactApp/static/css/main.ae30d9d7.chunk.css"
+  },
+  {
+    "revision": "6a84c7da698c8cf8a3b7",
+    "url": "/todoReactApp/static/js/2.4510165c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoReactApp/static/js/2.4510165c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8617376c5734e5aac9",
+    "url": "/todoReactApp/static/js/main.1086d605.chunk.js"
+  },
+  {
+    "revision": "9384ea0b7035044eb1c9",
+    "url": "/todoReactApp/static/js/runtime-main.b423b302.js"
+  }
+]);
